880a7236a04e05a5862af2e5d62c78fc582deb0e
doucet <homedoucetpaul@gmail.com>
2022-01-17 02:56:08 -0500

    add jupyter notebook, add correlation matrix plotting

--- build.sbt
+++ build.sbt
@@ -4,4 +4,4 @@ version := "0.1"
 
 scalaVersion := "2.13.8"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.0.2"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.2.0"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,27 @@
+import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
  * @author Paul Doucet (316442)
  */
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello Word")
+    System.setProperty("hadoop.home.dir", "C:/winutils/")
+
+    val spark = SparkSession
+      .builder()
+      .appName("flight-delay-prediction")
+      .config("spark.master", "local")
+      .getOrCreate()
+
+
+    val df = spark.read.option("header", "true").csv("datasets/2009.csv")
+
+    val columns = df.columns
+
+    df.show()
+    //val first = df.first()
+
+    //println(first)
   }
 }
