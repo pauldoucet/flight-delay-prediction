2c16e8a7cf8d21a1533c8e25cd9e2ca9cd36bc1a
doucet <homedoucetpaul@gmail.com>
2022-01-27 23:04:26 -0500

    adds AWS EMR support

--- build.sbt
+++ build.sbt
@@ -2,10 +2,10 @@ name := "flight-delay-prediction"
 
 version := "0.1"
 
-scalaVersion := "2.13.8"
+scalaVersion := "2.12.10"
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-sql" % "3.2.0",
-  "org.apache.spark" %% "spark-core" % "3.2.0",
-  "org.apache.spark" %% "spark-mllib" % "3.2.0"
+  "org.apache.spark" %% "spark-sql" % "2.4.8",
+  "org.apache.spark" %% "spark-core" % "2.4.8",
+  "org.apache.spark" %% "spark-mllib" % "2.4.8"
 )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,9 @@
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.evaluation.RegressionEvaluator
 import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.ml.param.ParamMap
 import org.apache.spark.ml.regression.LinearRegression
-import org.apache.spark.ml.tuning.{CrossValidator, TrainValidationSplit}
-import org.apache.spark.mllib.classification.SVMWithSGD
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.col
-import org.apache.spark.sql.{SQLContext, SparkSession}
 
 /**
  * @author Paul Doucet (316442)
@@ -18,13 +15,14 @@ object Main {
     val spark = SparkSession
       .builder()
       .appName("flight-delay-prediction")
-      .config("spark.master", "local")
+      .config("spark.master", "local[*]")
       .getOrCreate()
 
 
     val feature_cols = Array("DEP_TIME", "DEP_DELAY", "TAXI_OUT")
 
     val df = spark.read.option("header", "true")
+      //.csv("s3://flight-delays-analytic/datasets/2009.csv")
       .csv("datasets/2009.csv")
       .withColumn("DEP_TIME", col("DEP_TIME").cast("double"))
       .withColumn("DEP_DELAY", col("DEP_DELAY").cast("double"))
@@ -62,6 +60,7 @@ object Main {
 
     println(s"model got r^2 = $r2")
 
-    predicted.show()
+    System.in.read
+    spark.stop // spark --> SparkSession
   }
 }
